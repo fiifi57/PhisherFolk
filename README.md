eecbed07fc03158c804851b6d9b23c313a3222f1
fiifi57 <fiifi57@users.noreply.github.com>
2020-04-12 03:17:04 -0400

    update2

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PhisherFolk
-This repo uses packages and modules from the WordNet Project. The goal is to develop lexicons for additional features in the NELA Toolkit.
+This repo uses packages and modules from the WordNet Project. The goal is to develop lexicons for additional features to be added to the NELA Toolkit.
 However, it can be used for any lexicon-development project.
 
 ## Resources:
 1. <a target="_blank" href="https://www.nltk.org/howto/wordnet.html ">WordNet Portal </a>
-2. <a target="_blank" href="http://nelatoolkit.science/">Go to NELA Toolkit's website</a>
+2. <a target="_blank" href="http://nelatoolkit.science/">NELA Toolkit Portal</a>
--- modules/.ipynb_checkpoints/phisherfolk-checkpoint.py
+++ modules/.ipynb_checkpoints/phisherfolk-checkpoint.py
@@ -24,37 +24,59 @@ class phf:
         root_word - the main word for which you are trying to find synonyms
         level - how deep down the tree you want the search to go (range is 3)
         '''
-
-        # get synonyms of root_word
-        level_1 = []
-        for syn in wn.synsets(root_word): 
-            for le in syn.lemmas(): 
-                level_1.append(le.name()) 
-
-        # get all related words of synonyms in level_1
-        level_2 = []
-        for l1 in set(level_1):
-            for syn in wn.synsets(l1): 
-                for le in syn.lemmas(): 
-                    level_2.append(le.name())
-
-        # get all related words level_2 (include nouns, verbs, etc)
-        level_3a = []
-        for l2 in level_2:
-            for syn in wn.synsets(l2):
-                level_3a.append(syn)
-
-        level_3b = []
-        for i, syn in enumerate(level_3a):
-            level_3b.append(syn.lemma_names())
-            level_3c = ",".join([item for sublist in level_3b for item in sublist])
-            level_3c = level_3c.split(",")
-
+        
         if level == 0:
             return root_word
+        
+        
         if level == 1:
+            # get synonyms of root_word
+            level_1 = []
+            for syn in wn.synsets(root_word): 
+                for le in syn.lemmas(): 
+                    level_1.append(le.name()) 
             return set(level_1)
-        elif level == 2:
+                
+        if level == 2:
+            # get synonyms of root_word
+            level_1 = []
+            for syn in wn.synsets(root_word): 
+                for le in syn.lemmas(): 
+                    level_1.append(le.name()) 
+
+            # get all related words of synonyms in level_1
+            level_2 = []
+            for l1 in set(level_1):
+                for syn in wn.synsets(l1): 
+                    for le in syn.lemmas(): 
+                        level_2.append(le.name())
             return set(level_2)
-        elif level == 3:
+        
+        
+        if level == 3:                 
+            # get synonyms of root_word
+            level_1 = []
+            for syn in wn.synsets(root_word): 
+                for le in syn.lemmas(): 
+                    level_1.append(le.name()) 
+
+            # get all related words of synonyms in level_1
+            level_2 = []
+            for l1 in set(level_1):
+                for syn in wn.synsets(l1): 
+                    for le in syn.lemmas(): 
+                        level_2.append(le.name())
+
+            # get all related words level_2 (include nouns, verbs, etc)
+            level_3a = []
+            for l2 in level_2:
+                for syn in wn.synsets(l2):
+                    level_3a.append(syn)
+
+            level_3b = []
+            for i, syn in enumerate(level_3a):
+                level_3b.append(syn.lemma_names())
+                level_3c = ",".join([item for sublist in level_3b for item in sublist])
+                level_3c = level_3c.split(",")
+
             return set(level_3c)
